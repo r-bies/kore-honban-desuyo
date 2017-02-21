4bbd4c7416703c7b000656a1a8031b1eb55dc2f2
Takuhiro Yoshida <yoshida.takuhiro@runners.co.jp>
2017-02-21 15:32:21 +0900

    Display warning if the console is for global.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
  */
 $(function() {
     console.log($(".nav-elt-label").text());
-    re = /東京/;
-    if($(".nav-elt-label").text().match(re)) {
+    var tokyo = /東京/;
+    var global = /グローバル/;
+    if($(".nav-elt-label").text().match(tokyo)) {
         $("#h").after('<div style="background-color:#FF0000;text-align:center;"><h3>これ本番ですよ！これ本番ですよ！これ本番ですよ！これ本番ですよ！これ本番ですよ！これ本番ですよ！</h3></div>');
+    } else if($(".nav-elt-label").text().match(global)) {
+        $("#h").after('<div style="background-color:#FFFF00;text-align:center;"><h3>これグローバルですよ！これグローバルですよ！これグローバルですよ！これグローバルですよ！これグローバルですよ！これグローバルですよ！</h3></div>');
     }
 });
